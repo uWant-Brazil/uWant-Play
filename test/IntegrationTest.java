b725a5428b2b3ddd67470f070f8c40bd77cd593c
Felipe Bonezi <felipebonezi@gmail.com>
2014-06-01 21:29:31 -0300

    Teste de integração
    Teste para realizar cadastro de usuário através de redes sociais.

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,6 +1,7 @@
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import controllers.AbstractApplication;
+import models.classes.SocialProfile;
 import models.classes.User;
 import org.junit.Test;
 import play.core.j.JavaResultExtractor;
@@ -41,6 +42,51 @@ public class IntegrationTest {
                 body.put(AbstractApplication.ParameterKey.BIRTHDAY, birthday);
                 body.put(AbstractApplication.ParameterKey.GENDER, gender);
                 body.put(AbstractApplication.ParameterKey.MAIL, "mail-" + uniqueIdentifier + "@uwant-test.com.br");
+
+                FakeRequest fakeRequest = new FakeRequest(POST, "/v1/mobile/user/register").withJsonBody(body);
+                Result result = route(fakeRequest);
+
+                boolean status = (status(result) == Http.Status.OK);
+
+                assertThat(result).isNotNull();
+                assertThat(status).isTrue();
+
+                String responseBody = new String(JavaResultExtractor.getBody((SimpleResult) result));
+                JsonNode jsonResponse = Json.parse(responseBody);
+
+                assertStatusMessage(jsonResponse, status);
+            }
+
+        });
+    }
+
+    public void successfulRegisterWithSocialTest() {
+        running(fakeApplication(), new Runnable() {
+
+            @Override
+            public void run() {
+                Calendar now = Calendar.getInstance();
+                now.set(Calendar.YEAR, (1930 + (int)(Math.random() * (now.get(Calendar.YEAR) - 1930) + 1)));
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+                String birthday = formatter.format(now.getTime());
+
+                int gender = (now.getTimeInMillis() % 8 < 5 ? User.Gender.MALE.ordinal() : User.Gender.FEMALE.ordinal());
+
+                String uniqueIdentifier = UUID.randomUUID().toString();
+
+                ObjectNode body = Json.newObject();
+                body.put(AbstractApplication.ParameterKey.LOGIN, "test" + uniqueIdentifier);
+                body.put(AbstractApplication.ParameterKey.PASSWORD, uniqueIdentifier);
+                body.put(AbstractApplication.ParameterKey.FULL_NAME, "Teste " + uniqueIdentifier);
+                body.put(AbstractApplication.ParameterKey.BIRTHDAY, birthday);
+                body.put(AbstractApplication.ParameterKey.GENDER, gender);
+                body.put(AbstractApplication.ParameterKey.MAIL, "mail-" + uniqueIdentifier + "@uwant-test.com.br");
+
+                ObjectNode jsonSocialProfile = Json.newObject();
+                jsonSocialProfile.put(AbstractApplication.ParameterKey.SOCIAL_PROVIDER, SocialProfile.Provider.FACEBOOK.ordinal());
+                jsonSocialProfile.put(AbstractApplication.ParameterKey.TOKEN, uniqueIdentifier);
+
+                body.put(AbstractApplication.ParameterKey.SOCIAL_PROFILE, jsonSocialProfile);
 
                 FakeRequest fakeRequest = new FakeRequest(POST, "/v1/mobile/user/register").withJsonBody(body);
                 Result result = route(fakeRequest);
