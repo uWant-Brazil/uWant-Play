7caf74cd64cebc57c109daf3143a0b04055a917f
Felipe Bonezi <felipebonezi@gmail.com>
2014-06-07 10:14:06 -0300

    Factory
    Correção no factory do CDN para implementação do padrão de projeto
    SINGLETON.

--- app/models/cdn/CDNFactory.java
+++ app/models/cdn/CDNFactory.java
@@ -6,6 +6,20 @@ import models.AbstractFactory;
  * Created by Cleibson Gomes on 27/05/14.
  */
 public class CDNFactory extends AbstractFactory<CDNType, ICDN>{
+
+    /**
+     * Singleton para o factory.
+     */
+    private static CDNFactory INSTANCE;
+
+    private CDNFactory() {
+        // Do nothing...
+    }
+
+    public static CDNFactory getInstance() {
+        return (INSTANCE == null ? (INSTANCE = new CDNFactory()) : INSTANCE);
+    }
+
     @Override
     public ICDN get(CDNType id) {
         ICDN icdn;
