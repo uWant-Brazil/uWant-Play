1e4ff9cb546a62942ad706c5f77607029802ea42
Felipe Bonezi <felipebonezi@gmail.com>
2014-06-11 21:43:34 -0300

    Comment - CDN

--- app/utils/CDNUtil.java
+++ app/utils/CDNUtil.java
@@ -8,16 +8,30 @@ import models.classes.Multimedia;
 import java.io.File;
 
 /**
- * Created by felipebenezi on 08/06/14.
+ * Classe utilitária para ações relacionadas a entidades enviadas a CDN para armanezamento.
  */
 public abstract class CDNUtil {
 
+    /**
+     * CDN utilizada como default para o envio quando não especificado.
+     */
     private static final CDNType DEFAULT_TYPE = CDNType.AMAZON_S3;
 
+    /**
+     * Método para envio de arquivo para a CDN padrão - Amazon S3.
+     * @param file
+     * @return multimedia
+     */
     public static Multimedia sendFile(File file) {
         return sendFile(DEFAULT_TYPE, file);
     }
 
+    /**
+     * Método para envio de arquivo para CDN específicada.
+     * @param type
+     * @param file
+     * @return multimedia
+     */
     public static Multimedia sendFile(CDNType type, File file) {
         CDNFactory factory = CDNFactory.getInstance();
         ICDN icdn = factory.get(type);
