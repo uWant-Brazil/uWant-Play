7d5ee68180d036d253660a74d6412a6f333bcbfe
Felipe Bonezi <felipebonezi@gmail.com>
2014-06-11 21:30:42 -0300

    Comment - Regex
    Comentário da classe de Regex.

--- app/utils/RegexUtil.java
+++ app/utils/RegexUtil.java
@@ -3,12 +3,18 @@ package utils;
 import java.util.regex.Pattern;
 
 /**
- * Created by felipebonezi on 22/05/14.
+ * Classe utilitária para ações relacionadas a utilização de Regex.
  */
 public abstract class RegexUtil {
 
+    /**
+     * Regex responsável por validar uma String com formatação de e-mail.
+     */
     private static final String MAIL_REGEX = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
 
+    /**
+     * Método responsável por validar uma String com formatação de e-mail.
+     */
     public static boolean isValidMail(String mail) {
         return mail.matches(MAIL_REGEX);
     }
