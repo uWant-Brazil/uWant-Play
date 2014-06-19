b640a233a48842f75150002ac119a1b09287005a
Felipe Bonezi <felipebonezi@gmail.com>
2014-06-18 22:01:22 -0300

    Google Cloud Message
    Integração com o serviço vinculado a nossas classes.

--- app/models/cloud/GoogleCloudMessage.java
+++ app/models/cloud/GoogleCloudMessage.java
@@ -1,7 +1,11 @@
 package models.cloud;
 
 import models.classes.Mobile;
+import models.cloud.gcm.Message;
+import models.cloud.gcm.Sender;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,9 +13,41 @@ import java.util.List;
  */
 public class GoogleCloudMessage implements INotificationService {
 
+    /**
+     * Time-to-live durante o envio dos pacotes.
+     */
+    private static final int MAX_TTL = 20;
+
+    /**
+     * Tentativas máximas de envio da notificação.
+     */
+    private static final int MAX_RETRIES = 10;
+
+    /**
+     * Chave de autenticação nos servidores do Google Inc.
+     */
+    private static final String API_KEY = "";
+
     @Override
     public void push(List<Mobile> mobiles) {
+        Message message = new Message.Builder()
+                .timeToLive(MAX_TTL)
+                .delayWhileIdle(true)
+                .build();
 
+        List<String> identifiers = new ArrayList<String>(mobiles.size());
+        for (Mobile mobile : mobiles) {
+            String identifier = mobile.getIdentifier();
+            identifiers.add(identifier);
+        }
+
+        Sender sender = new Sender(API_KEY);
+        try {
+            sender.send(message, identifiers, MAX_RETRIES);
+        } catch (IOException e) {
+            e.printStackTrace();
+            // TODO Tratamento do erro...
+        }
     }
 
 }
